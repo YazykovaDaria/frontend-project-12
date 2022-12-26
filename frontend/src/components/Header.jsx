0b2d5a1ac7b71d42e39470a883be903f000033df
YazykovaDaria <yazykovadasva@gmail.com>
2022-12-26 14:21:47 +0300

    feat: add logout btn

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,22 +1,35 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../context/contextHooks';
 
-const Header = () => (
-  <>
-    <header>
-      <ul>
-        <li>
-          <Link to="/">Hexlet chat</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Registration</Link>
-        </li>
-      </ul>
-    </header>
-    <Outlet />
-  </>
-);
+const Header = () => {
+  const { user, logOut } = useAuth();
+  const navigate = useNavigate();
+
+  const getLogOut = () => {
+    logOut();
+    navigate('/login');
+  };
+
+  return (
+    <>
+      <header className="d-flex justify-content-between">
+        <ul>
+          <li>
+            <Link to="/">Hexlet chat</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Registration</Link>
+          </li>
+        </ul>
+        {user ? <Button onClick={getLogOut}>Выйті</Button> : null}
+      </header>
+      <Outlet />
+    </>
+  );
+};
 
 export default Header;
