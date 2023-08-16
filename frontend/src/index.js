f9735d3276f3a9a1c166a2470a804474d0115f3e
YazykovaDaria <yazykovadasva@gmail.com>
2023-08-16 11:02:13 +0300

    feat: internationlization

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+// eslint-disable-next-line no-unused-vars
+import i18n from './i18next/i18n';
+import GoApp from './goApp';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <GoApp />
     </BrowserRouter>
   </React.StrictMode>,
 );
